509afafe97bf95f0868fd84ac22c4d6b44e15fed
Alex <aleksan672@gmail.com>
2022-11-11 21:27:33 +0300

    Added solution1

---
+++ solution1/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("Enter two integers --> ");
+
+int number1 = Convert.ToInt32(Console.ReadLine());
+int number2 = Convert.ToInt32(Console.ReadLine());
+
+if(number1 == number2*number2)
+{
+    Console.WriteLine("Yes");
+}
+else
+{
+    Console.WriteLine("No");
+}
