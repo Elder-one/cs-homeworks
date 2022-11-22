5f27ec55c9497dde2d851a0ea03e44acae31fbf2
Alex <aleksan672@gmail.com>
2022-11-22 21:10:37 +0300

    Added problem023

---
+++ problem023/Program.cs
@@ -0,0 +1,16 @@
+﻿void printSchedule(int N)
+{
+    if(N > 0)
+    {
+        for(int i = 1; i < N + 1; i++)
+        {
+            Console.WriteLine($"{i} | {i*i*i}");
+        }
+    }
+    else Console.WriteLine("Your number is out of range");
+}
+
+Console.Write("Enter an integer more than 0 --> ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+printSchedule(number);
