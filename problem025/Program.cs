d513b18e1494b6c4626c18e0dc899798c2fe413a
Alex <aleksan672@gmail.com>
2022-11-23 19:44:14 +0300

    Added problem025

---
+++ problem025/Program.cs
@@ -0,0 +1,23 @@
+﻿int Power(int number, int power)
+{
+    int result = 1;
+    for(int i = 0; i < power; i++)
+    {
+        result *= number;
+    }
+    return result;
+}
+
+
+Console.Write("Enter an integer --> ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Enter the power more than 0 --> ");
+int pow = Convert.ToInt32(Console.ReadLine());
+
+if(pow > 0)
+{
+    int result = Power(num, pow);
+    Console.WriteLine($"{num}**{pow} = {result}");
+}
+else Console.WriteLine("Your power is out of range");
