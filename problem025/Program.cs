fcd11045aebcf15eb86d447b9ec44e2a7ad9f66a
Alex <aleksan672@gmail.com>
2022-11-23 19:46:21 +0300

    Added description for problem025

--- problem025/Program.cs
+++ problem025/Program.cs
@@ -1,4 +1,12 @@
-﻿int Power(int number, int power)
+﻿//  Напишите цикл, который принимает на вход
+//  два числа (A и B) и возводит
+//  число A в натуральную степень B.
+
+// 3, 5 -> 243
+
+// 2, 4 -> 16
+
+int Power(int number, int power)
 {
     int result = 1;
     for(int i = 0; i < power; i++)
